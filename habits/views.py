4035c9d3860fb877b5190a7ed14399751fb5624d
Natalia Bazhina <nataliaagapova27@yandex.ru>
2025-07-22 00:16:15 +0300

    first commit

---
+++ habits/views.py
@@ -0,0 +1,23 @@
+from rest_framework.viewsets import ModelViewSet
+from habits.models import Habit
+from habits.serializers import HabitSerializer
+
+
+class HabitViewSet(ModelViewSet):
+    """
+       ViewSet для работы с привычками пользователя.
+       Обеспечивает стандартные CRUD-операции (создание, чтение, обновление, удаление).
+       Все операции доступны только для аутентифицированных пользователей.
+       Пользователь видит и может изменять только свои привычки.
+       """
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+    def get_queryset(self):
+        """ Возвращает привычки текущего пользователя"""
+        return super().get_queryset().filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        """Создает новую привычку, автоматически привязывая ее к текущему пользователю"""
+
+        serializer.save(user=self.request.user)
